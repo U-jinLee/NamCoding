93beb6c96ce55b7691dc97263c730cc4358b27ee
Yoojin Lee <yoojinLee.dev@gmail.com>
2021-11-22 23:41:21 +0900

    feat: VarEx2 commit
    - 변수 저장에 관련 예시 저장

--- src/main/java/variable/VarEx2.java
+++ src/main/java/variable/VarEx2.java
@@ -4,13 +4,13 @@ public class VarEx2 {
     public static void main(String[] args) {
         int x = 10, y= 20;
         int tmp = 0;
-        System.out.printf("x:%d,y:%d", x, y);
+        System.out.printf("Before x:%d,y:%d", x, y);
 
         System.out.println("");
 
         tmp = x;
         x = y;
         y= tmp;
-        System.out.printf("x:%d,y:%d", x, y);
+        System.out.printf("After x:%d,y:%d", x, y);
     }
 }
